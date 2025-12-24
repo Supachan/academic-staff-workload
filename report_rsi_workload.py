716a31938af42aae9f56047bf08bbe6995a1fd75
Supachan_Joke <supachan.t@gmail.com>
2025-12-24 14:31:10 +0700

    added main function

--- report_rsi_workload.py
+++ report_rsi_workload.py
@@ -16,54 +16,63 @@ matplotlib.rc('font', family='Sarabun')
 
 from rsi_modules import *
 
+def main_inside():
+    mm3_url         = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=1475905714"
+    publication_url = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=2125519370"
+    theses_url      = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=609352385"
 
-mm3_url         = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=1475905714"
-publication_url = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=2125519370"
-theses_url      = "https://docs.google.com/spreadsheets/d/1NrR5_OKyydIklO2UPBwHYdqI0Y_x1CaQxrie4OTFaDs/export?format=csv&gid=609352385"
+    F = pd.read_csv(mm3_url)
+    publication = pd.read_csv(publication_url).fillna("")
+    theses = pd.read_csv(theses_url).fillna("")
+    theses = theses.drop_duplicates(subset=['รหัส (นักศึกษา)'], keep='last')
 
-F = pd.read_csv(mm3_url)
-publication = pd.read_csv(publication_url).fillna("")
-theses = pd.read_csv(theses_url).fillna("")
-theses = theses.drop_duplicates(subset=['รหัส (นักศึกษา)'], keep='last')
+    # website:
+    st.title('ภาระงานของคณาจารย์ในสถาบันราชสุดาปีการศึกษา 2568')
 
-# website:
-st.title('ภาระงานของคณาจารย์ในสถาบันราชสุดาปีการศึกษา 2568')
+    st.subheader("1. ชั่วโมงสอนและจำนวนวิชาที่รับผิดชอบ")
 
-st.subheader("1. ชั่วโมงสอนและจำนวนวิชาที่รับผิดชอบ")
+    academic_year = st.radio(
+        "เลือกเทอมและปีการศึกษา",
+        ["2568/1", "2568/2"],
+    )
 
-academic_year = st.radio(
-    "เลือกเทอมและปีการศึกษา",
-    ["2568/1", "2568/2"],
-)
+    if academic_year == "2568/1":
+        start_date = '8/5/2025'
+        end_date   = '9/19/2025'
+    elif academic_year == "2568/2":
+        start_date = '12/12/2025'
+        end_date   = '12/30/2025'
 
-if academic_year == "2568/1":
-    start_date = '8/5/2025'
-    end_date   = '9/19/2025'
-elif academic_year == "2568/2":
-    start_date = '12/12/2025'
-    end_date   = '12/30/2025'
+    f = F[F['Timestamp'].between(start_date, end_date)]
+    f = f.drop_duplicates(subset=['รหัสวิชา'], keep='last')
+    t1,t2 = report_mm3(f)
 
-f = F[F['Timestamp'].between(start_date, end_date)]
-f = f.drop_duplicates(subset=['รหัสวิชา'], keep='last')
-t1,t2 = report_mm3(f)
+    st.subheader("2. ผลงานตีพิมพ์ทางวิชาการในแต่ละปี")
+    publication,df = get_publication(publication)
+    df = df.reset_index().rename(columns={'index':'ปี'})
+    st.write(df)
 
-st.subheader("2. ผลงานตีพิมพ์ทางวิชาการในแต่ละปี")
-publication,df = get_publication(publication)
-df = df.reset_index().rename(columns={'index':'ปี'})
-st.write(df)
+    st.subheader("3. ภาระงานควบคุมวิทยานิพนธ์")
+    B = pd.DataFrame(get_thesis_workload_new(theses)).T
+    st.write(B)
 
-st.subheader("3. ภาระงานควบคุมวิทยานิพนธ์")
-B = pd.DataFrame(get_thesis_workload_new(theses)).T
-st.write(B)
+    st.subheader("4. ภาพรวมความก้าวหน้าวิทยานิพนธ์ของนักศึกษา")
+    N = get_thesis_progression_summary(theses)
+    A = pd.concat(N, axis=1)
+    st.write(A)
+    st.write("หมายเหตุ:")
+    st.write("MA  = หส.ปริญญาโท สาขาคุณภาพชชีวิตคนพิการ")
+    st.write("MEd = หส.ปริญญาโท สาขาการศึกษาพิเศษ")
+    st.write("PhD = หส.ปรัชญาดุษฎีบัณฑิต สาขาคุณภาพชีวิตคนพิการ")
 
-st.subheader("4. ภาพรวมความก้าวหน้าวิทยานิพนธ์ของนักศึกษา")
-N = get_thesis_progression_summary(theses)
-A = pd.concat(N, axis=1)
-st.write(A)
-st.write("หมายเหตุ:")
-st.write("MA  = หส.ปริญญาโท สาขาคุณภาพชชีวิตคนพิการ")
-st.write("MEd = หส.ปริญญาโท สาขาการศึกษาพิเศษ")
-st.write("PhD = หส.ปรัชญาดุษฎีบัณฑิต สาขาคุณภาพชีวิตคนพิการ")
+    # pd.DataFrame.to_csv(t1,'teaching_hours.csv',index=False)
+    # pd.DataFrame.to_csv(t2,'teaching_courses_responsibility.csv',index=False)
 
-# pd.DataFrame.to_csv(t1,'teaching_hours.csv',index=False)
-# pd.DataFrame.to_csv(t2,'teaching_courses_responsibility.csv',index=False)
+def main():
+    # solution: https://discuss.streamlit.io/t/there-is-a-way-to-make-my-webapp-continuously-active/27556
+    blabla=True
+    while blabla == True:
+        main_inside()
+
+if __name__ == "__main__":
+    main()
